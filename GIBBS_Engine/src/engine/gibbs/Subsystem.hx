a868513394a3cd1aa9f5d068f0296c5f341f3ac6
David Yu <lewen.yu@gmail.com>
2013-07-08 00:05:21 -0400

    compile GIBBS_Engine

--- GIBBS_Engine/src/engine/gibbs/Subsystem.hx
+++ GIBBS_Engine/src/engine/gibbs/Subsystem.hx
@@ -1,18 +1,22 @@
 package engine.gibbs;
 import engine.gibbs.subsystems.EntityObserver;
 
-class Subsystem extends EntityObserver
+class Subsystem implements EntityObserver
 {
-	public function new() {
-		
-	}
-	
-	public function onAdded( e : Entity ) : Void {
-	}
-	
-	public function onDeleted( e : Entity ) : Void {
-		
-	}
-	
-	var entities : List<Entity>;
+    public function new() {
+
+    }
+
+    public function onAdded( e : Entity ) : Void {
+    }
+
+    public function onDeleted( e : Entity ) : Void {
+
+    }
+
+    public function onAttachCmp( e : Entity, cmp : Component ) : Void {
+
+    }
+
+    var entities : List<Entity>;
 }
