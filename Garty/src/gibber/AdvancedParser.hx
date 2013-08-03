8f7090cfdd1487ce37643c87367ed6d796c5c5a2
David Yu <lewen.yu@gmail.com>
2013-08-03 02:36:22 -0400

    Hacked together move cmd WIP in intelligent commander

--- Garty/src/gibber/AdvancedParser.hx
+++ Garty/src/gibber/AdvancedParser.hx
@@ -10,6 +10,7 @@ import com.artemisx.ComponentMapper;
 import com.artemisx.ComponentMapper;
 import gibber.components.PosCmp;
 import gibber.components.ContainerCmp;
+import utils.Vec2;
 
 class AdvancedParser
 {
@@ -25,7 +26,25 @@ class AdvancedParser
         var words = command.split( " " );
         
         switch( words[0] ) {
+
             case "go":
+                
+                if ( words.length > 1 ) {
+                    switch ( words[1] ) {
+                        case "-v", "-vec": //directly go to this new position
+                            if ( words.length == 4) {
+                                var newLoc = new Vec2( Std.parseFloat( words[2] ), Std.parseFloat( words[3] ) );
+                                god.commander.goToPosition( god.player, newLoc );
+                                return "";
+                            } else {
+                                god.debugPrintln( "Usage: go " + words[1] + " x y " );
+                            }
+                        default:
+                            god.debugPrintln( "I don't understand that flag for 'go.'");
+                            return "";
+                    }
+                }
+
                 var dest : Entity = null;
                 dest = god.world.getManager( NameRegistry ).getEntity( words[1] );
                 god.world.getManager( NameRegistry ).getEntity( words[1] );
--- Garty/src/gibber/Commander.hx
+++ Garty/src/gibber/Commander.hx
@@ -1,10 +1,14 @@
 package gibber;
 import com.artemisx.Entity;
 import com.artemisx.World;
+import gibber.commands.Command;
 import gibber.components.ContainerCmp;
+import gibber.components.CmdQueue;
+import gibber.components.PosCmp;
 import gibber.components.LookCmp;
 import gibber.components.PortalCmp;
 import gibber.managers.NameRegistry;
+import utils.Vec2;
 
 using Lambda;
 
@@ -12,6 +16,13 @@ class Commander
 {
     public function new( g : God ) {
         god = g;
+    }
+
+    public function goToPosition( mover : Entity, newLoc : Vec2 ) : Void {
+        var curSector : Entity   = mover.getComponent( PosCmp ).sector;
+        var moveCmd   : Command  = god.cmdFactory.createCmd( "move", [ mover, newLoc, curSector ] );
+        var cq        : CmdQueue = mover.getComponent( CmdQueue );
+        cq.enqueue( moveCmd );
     }
 
     public function goToSector( mover : Entity, destSectorName : String ) : Void {
