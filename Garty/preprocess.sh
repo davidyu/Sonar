8db006c20182762dfbbcf893c9ba24adfde68e0c
David Yu <lewen.yu@gmail.com>
2013-08-10 01:46:31 -0400

    Add first-pass preprocessor Mac/Unix shell script with makefile, need to port to Windows or Python

---
+++ Garty/preprocess.sh
@@ -0,0 +1,51 @@
+#############################################################################
+#
+# Gibbs JSON deserializing preprocessor
+#
+#   Goes through the src directory and outputs for all files, the following:
+#
+#       <classname> <classpath>
+#
+#   which is then loaded by our JSON deserializer to resolve classpaths from
+#   class names
+#
+#############################################################################
+
+#!/bin/sh
+SRC_PATH=./src
+
+# get into source path
+cd $SRC_PATH
+
+# get all .hx source files
+hxfiles=$(find . -regex ".*\.hx")
+for file in $hxfiles
+do
+    ####################
+    # get classname
+    ####################
+
+    # get base filename
+    classname=${file##*/}
+
+    # trim ending '.hx'
+    classname=${classname%\.*}
+
+    ####################
+    # get classpath
+    ####################
+
+    # trim beginning './'
+    cp=${file##\./}
+
+    # trim ending '.hx'
+    cp=${cp%\.*}
+
+    # replace all instances of '/' with '.'
+    cp=$(echo $cp | sed -e "s/\//\./g")
+
+    echo $classname $cp
+done
+
+# go back ( pipe to /dev/null because otherwise it outputs working directory path )
+cd - >/dev/null
