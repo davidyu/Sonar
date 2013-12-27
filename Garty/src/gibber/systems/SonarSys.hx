54e7aa910a247367bfac8218b5a3764cc7abe52b
David Yu <lewen.yu@gmail.com>
2013-12-26 22:28:48 -0500

    remove degreeDiff and clean up traces

--- Garty/src/gibber/systems/SonarSys.hx
+++ Garty/src/gibber/systems/SonarSys.hx
@@ -63,16 +63,12 @@ class SonarSys extends EntitySystem
                             //trace( 'performing intersection test with { c : $center, r : $radius } and { a : ${p.verts[k]}, b : ${p.verts[k + 1]} }' );
                             switch ( intersect ) {
                                 case Line( p, q ):
-                                    trace( "-----------new intersect------------" );
-                                    trace( "line:" + p + ", " + q );
-                                    trace( "center:" + center );
                                     var ranges : Array<Range> = new Array<Range>(); // this will be the new set of culled ranges we add to sonar.cullRanges
 
                                     var rangeStart = pointToRadian( center, p );
                                     var rangeEnd   = pointToRadian( center, q );
 
                                     ranges.push( { start: rangeStart, end: rangeEnd } );
-                                    trace( "range:" + Math2.radToDeg( rangeStart ) + ", " + Math2.radToDeg( rangeEnd ) );
 
                                     // compute culling for ranges
                                     for ( orng in sonar.cullRanges ) {
@@ -97,6 +93,10 @@ class SonarSys extends EntitySystem
                                                 ranges.remove( rng );
                                                 ranges.push( { start: rng.start, end: orng.start } );
                                                 ranges.push( { start: orng.end, end: rng.end } );
+                                            //rng    -----
+                                            //orng   -----
+                                            } else if ( Math.abs( radianDiff( orng.start, rng.start ) ) < Math2.EPSILON && Math.abs( radianDiff( orng.end, rng.end ) ) < Math2.EPSILON ) {
+                                                ranges.remove( rng );
                                             }
                                         }
                                     }
@@ -113,19 +113,21 @@ class SonarSys extends EntitySystem
                                             // throw "range a should have been vetted!";
                                         }
 #end
-                                        return Math2.sign( a.start - b.start );
+                                        return -Math2.sign( radianDiff( a.start, b.start ) );
                                     } );
 
                                     for ( rng in ranges ) {
+
+                                        trace( "-------ranges to draw---------: " );
+                                        trace( Math2.radToDeg( rng.start ) + " " + Math2.radToDeg( rng.end ) );
                                         function radianToPoint( origin, theta, invertedY : Bool = true ) : Vec2 {
                                             var direction = new Vec2( Math.sin( theta ), invertedY ? -Math.cos( theta ) : Math.cos( theta ) );
 
-                                            trace( "reconstructed vector: " + direction );
                                             switch ( Math2.getRayLineIntersection( { origin: origin, direction: direction }, { a: p, b: q } ) ) {
                                                 case Point( point ): return point;
                                                 default:
-                                                    trace( Math2.getRayLineIntersection( { origin: center, direction: direction }, { a: p, b: q } ) );
-                                                    trace( direction );
+                                                    //trace( Math2.getRayLineIntersection( { origin: center, direction: direction }, { a: p, b: q } ) );
+                                                    //trace( direction );
                                                     return null;
                                             }
                                         }
@@ -161,16 +163,6 @@ class SonarSys extends EntitySystem
         var diff = b - a;
         if ( diff >  Math.PI ) diff -= Math.PI * 2; // as expected, b follows a CW, but we should go CCW for the smaller angle
         if ( diff < -Math.PI ) diff += Math.PI * 2; // edge case: going from a to b we pass the y-axis separating quadrants 1 and 4. So we apply a magical 360 error correcting factor
-        return diff;
-    }
-
-    // convenience; made testing easier
-    // returns f, where |f| is the shortest distance between a and b
-    // and f > 0 if b follows a clockwise and f < 0 if a follows b clockwise
-    private function degreeDiff( a: Float, b: Float ): Float {
-        var diff = b - a;
-        if ( diff >  180.0 ) diff -= 360.0; // as expected, b follows a CW, but we should go CCW for the smaller angle
-        if ( diff < -180.0 ) diff += 360.0; // edge case: going from a to b we pass the y-axis separating quadrants 1 and 4. So we apply a magical 360 error correcting factor
         return diff;
     }
 
