c6afa2f6bf1e7408c034c297e9c3d3ae96a1c910
David Yu <lewen.yu@gmail.com>
2013-12-29 16:39:36 -0500

    added bresenham line-drawing to augment trails

--- Garty/src/gibber/systems/RenderTrailSys.hx
+++ Garty/src/gibber/systems/RenderTrailSys.hx
@@ -20,6 +20,8 @@ import gibber.components.TimedEffectCmp;
 
 import utils.Vec2;
 
+using Lambda;
+
 class RenderTrailSys extends EntitySystem
 {
     public function new( root : MovieClip ) {
@@ -50,7 +52,8 @@ class RenderTrailSys extends EntitySystem
         var time : TimedEffectCmp;
         var trail : TrailCmp;
         var pos : PosCmp;
-        var screenPos : Vec2;
+        var lastScreenPos : Vec2;
+        var curScreenPos : Vec2;
 
         bmd.colorTransform( bmd.rect, fade ); // fade out every update
 
@@ -62,8 +65,54 @@ class RenderTrailSys extends EntitySystem
 
             // implement brensenham to draw line from prev pos to current?
             // there should be a helper function (maybe it exists already) to get abs coords of a posCmp
-            screenPos = pos.pos.add( posMapper.get( pos.sector ).pos ); // wow.gif
-            bmd.setPixel32( Std.int( screenPos.x ), Std.int( screenPos.y ), 0xffffffff );
+            lastScreenPos = pos.pos.add( posMapper.get( pos.sector ).pos ).sub( pos.dp );
+            curScreenPos = pos.pos.add( posMapper.get( pos.sector ).pos );
+            // bmd.setPixel32( Std.int( screenPos.x ), Std.int( screenPos.y ), 0xffffffff );
+
+            // pass this into bresenham
+            function plotPixelOnBmd( x: Int, y: Int ) {
+                bmd.setPixel32( x, y, 0xffffffff );
+            }
+
+            bresenham( Std.int( lastScreenPos.x ), Std.int( lastScreenPos.y ),
+                       Std.int( curScreenPos.x ) , Std.int( curScreenPos.y ) ,
+                       plotPixelOnBmd );
+        }
+    }
+
+    // very smart, complete, and well-optimized algorithm courtesy of wikipedia
+    private function bresenham( x0, y0, x1, y1, plot: Int->Int-> Void ) {
+        var dx : Float = Math.abs( x1 - x0 );
+        var dy : Float = Math.abs( y1 - y0 );
+
+        var xStep : Int = 0;
+        var yStep : Int = 0;
+
+        // this takes care of all possible slopes
+        if ( x0 < x1 ) xStep = 1 else xStep = -1;
+        if ( y0 < y1 ) yStep = 1 else yStep = -1;
+
+        var err : Float = dx - dy; // in the vanilla algorithm, if Math.abs(err) > 0.5 then we increment y by yStep
+
+        while ( true ) {
+            plot( x0, y0 );
+            if ( x0 == x1 && y0 == y1 ) break;
+
+            var e2 = err * 2;
+            if ( e2 > -dy ) { // -> 2dx > dy ( slope is greater than 1/2 )
+                err -= dy;
+                x0 += xStep;
+            }
+
+            if ( x0 == x1 && y0 == y1 ) {
+                plot( x0, y0 );
+                break;
+            }
+
+            if ( e2 < dx ) { // -> 2dy > dx ( slope is less than 2 )
+                err += dx;
+                y0 += yStep;
+            }
         }
     }
 
