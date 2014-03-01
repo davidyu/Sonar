b883898f664d8cd42e61ebed22b96b8502c2fd46
David Yu <lewen.yu@gmail.com>
2014-02-28 21:25:31 -0800

    fix server connect bug
    - make server respond to client connection immediately, instead of
      setting up a connect event listener.
    - fixes non-connect bug on my MBP 13""

--- relay/server.js
+++ relay/server.js
@@ -8,25 +8,7 @@ var gameServer = net.createServer( function ( socket ) {
   socket.name = socket.remoteAddress + ":" + socket.remotePort;
   socket.id = clients.length + 1; // start ids at 1
 
-  clients.push( socket );
-
-  console.log( "connected to client " + socket.name );
-
-  socket.on( 'connect', function( data ) {
-    console.log( "connect event" );
-    var clientID = new Buffer( [ 255, socket.id ] );
-    socket.write( clientID ); // send the client ID
-    var socketJoin = new Buffer( [ 254, socket.id ] );
-
-    // broadcast this client's joining to every other client
-    clients.forEach( function( client ) {
-      if ( client == socket ) return;
-      client.write( socketJoin );
-      var clientAdd = new Buffer( [ 254, client.id ] );
-      socket.write( clientAdd );
-    } );
-  } );
-
+  // set up some listeners for this socket
   socket.on( 'data', function( data ) {
     //console.log( "data of length " + data.readUInt16BE( 1 ) + ":|" + data.toString() + "|")
     if ( data == '<policy-file-request/>\0' ) {
@@ -40,6 +22,22 @@ var gameServer = net.createServer( function ( socket ) {
     clients.splice( clients.indexOf( socket ), 1 );
     // relay leave event
     console.log( "client " + socket.name + " left the game." );
+  } );
+
+  // acknowledge this socket's connection
+  clients.push( socket );
+  console.log( "connected to client " + socket.name );
+
+  var clientID = new Buffer( [ 255, socket.id ] );
+  socket.write( clientID ); // send the client ID
+  var socketJoin = new Buffer( [ 254, socket.id ] );
+
+  // broadcast this client's joining to every other client
+  clients.forEach( function( client ) {
+    if ( client == socket ) return;
+    client.write( socketJoin );
+    var clientAdd = new Buffer( [ 254, client.id ] );
+    socket.write( clientAdd );
   } );
 
   function relay( data, sender ) {
