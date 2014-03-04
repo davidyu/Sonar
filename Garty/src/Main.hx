cb8fc7f428f3aba1f6b8d254915c800824909fe5
David Yu <lewen.yu@gmail.com>
2014-03-04 02:11:23 -0500

    woo! CRT artifact shader
    - implemented simple CRT artifact shader where we have banded sampling
      of the channels so we can see constituent red and green
    - using orthographic camera so anti-aliasing artifacting is gone
    - finish #32

--- Garty/src/Main.hx
+++ Garty/src/Main.hx
@@ -13,6 +13,98 @@ import com.sociodox.theminer.TheMiner;
 
 using gibber.Util;
 
+class PostEffectsShader extends h3d.impl.Shader {
+#if flash 
+    static var SRC = {
+        var input : {
+            pos : Float3,
+            uv : Float2,
+        };
+
+        var tuv : Float2;
+
+        function vertex( mproj:Matrix ) {
+            out = input.pos.xyzw*mproj;
+            tuv =  input.uv;
+        }
+
+        function fragment( tex : Texture ) {
+            var c : Float4 = [ 0, 0, 0, 1. ];
+            c.r = tex.get([ tuv.x - 0.003, tuv.y ] ).r;
+            c.g = tex.get([ tuv.x, tuv.y ] ).g;
+            c.b = tex.get([ tuv.x + 0.003, tuv.y ] ).b;
+            out = c;
+        }
+    };
+
+#else
+static var VERTEX = "
+    attribute vec3 pos;
+    uniform mat4 mproj;
+    attribute vec2 uv;
+
+    varying vec2 tuv;
+
+    void main(void) {
+        gl_Position = vec4(pos,1)*mproj;
+        tuv = uv;
+    }";
+
+static var FRAGMENT = "
+    uniform sampler2D tex;
+    varying vec2 tuv;
+    void main(void) {
+        gl_FragColor = texture2D(tex, tuv);
+        //gl_FragColor = vec4(1,1,1,1);
+    }
+";
+#end
+}
+
+class PostEffectsMaterial extends h3d.mat.Material{
+    public var tex : h3d.mat.Texture;
+    var pshader : PostEffectsShader;
+
+    public function new(tex) {
+        this.tex = tex;
+        pshader = new PostEffectsShader();
+        depthTest = h3d.mat.Data.Compare.Always;
+        culling = None;
+
+        super( pshader );
+    }
+
+    override function setup( ctx : h3d.scene.RenderContext ) {
+        super.setup(ctx);
+        pshader.tex = tex;
+        pshader.mproj = ctx.camera.m;
+    }
+}
+
+class Screen extends CustomObject {
+
+    public var tex(get,set) : h3d.mat.Texture;
+    var sm : PostEffectsMaterial;
+
+    public function new( tex : h3d.mat.Texture, parent )
+    {
+        var prim = new h3d.prim.Cube();
+        prim.translate( -0.5, -0.5, -0.5);
+        prim.addUVs();
+        prim.addNormals();
+
+        super( prim, sm = new PostEffectsMaterial( tex ), parent );
+    }
+
+    function get_tex() {
+        return sm.tex;
+    }
+
+    function set_tex( v ) {
+        return sm.tex = v;
+    }
+}
+
 class Main 
 {
     static var engine : h3d.Engine;
@@ -21,7 +113,6 @@ class Main
     static var framebuffer : h2d.Sprite; // accumulator
     static var renderTarget : h2d.Tile; // intermediate
 	static var time : Float;
-    static var obj : h3d.scene.Mesh;
 
     static function update()
     {
@@ -49,6 +140,7 @@ class Main
             prim.translate( -0.5, -0.5, -0.5);
             prim.addUVs();
             prim.addNormals();
+
             var bmd = new hxd.BitmapData( p2( engine.width ), p2( engine.height ) );
             renderTarget = h2d.Tile.fromBitmap( bmd );
 
@@ -56,14 +148,26 @@ class Main
 
             // sanity check
             // var tex = h3d.mat.Texture.fromColor( 0xffffffff );
-            var mat = new h3d.mat.MeshMaterial( tex );
+            var screen = new Screen( tex, scene );
 
-            scene.camera.pos.set( 0, 0, 1.375 ); // ***
+            scene.camera.pos.set( 0, 0, 1. );
+
+            // make orthographic camera bounds just at the edges of the cube
+            scene.camera.orthoBounds = new h3d.col.Bounds();
+            scene.camera.orthoBounds.xMin = -0.5;
+            scene.camera.orthoBounds.yMin = -0.5;
+            scene.camera.orthoBounds.xMax =  0.5;
+            scene.camera.orthoBounds.yMax =  0.5;
+
             scene.camera.up.set( 0, -1, 0 );
             scene.camera.target.set( 0, 0, 0 );
+
             scene.camera.update();
 
-            obj = new Mesh( prim, mat, scene );
+            /*
+            scene.camera.pos.set( 0, 0, 1.375 ); // ***
+            scene.camera.update();
+            */
 
             framebuffer = new h2d.Sprite( backscene );
             framebuffer.x = 0;
